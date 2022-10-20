968540cb6bf79d43c89fd9599165aaba1069ce68
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-10-19 23:44:13 -0500

    Add files via upload

--- Homework 05/hw5.py
+++ Homework 05/hw5.py
@@ -17,83 +17,99 @@ def main(file):
         f = csv.reader(csv_file, delimiter=',')
         next(f)
         for row in f: ## For each row in the file
-            if row[0] != '"':
+            if row[0] != '"': ## If the row is not a header
                 year.append(int(row[0]))
-            if row[-1] != '-':
+            if row[-1] != '-': ## If the row is not a header
                 days.append(int(row[-1]))
 
-    s = Series(days,year) ## Create a series
+    s = Series(days, year) ## Create a series
     ax = s.plot.line() ## Plot the series
     ax.set_xlabel("Year") ## Set the x label
     ax.set_ylabel("Num of frozen days") ## Set the y label
 
-    plt.savefig("plot.jpg")
+    plt.savefig("plot.jpg") ## Save the plot
 
     #Q3a
     X = []
 
-    for x in year:
-        vector = [1,x]
-        vector = np.array(vector)
-        X.append(vector)
+    for x in year: ## For each year
+        vector = [1,x] ## Create a vector
+        vector = np.array(vector) ## Convert to numpy array
+        X.append(vector) ## Append to X
 
-    X = np.array(X)
-    print("Q3a:")
+    X = np.array(X) ## Convert to numpy array
+
+    ## Print the result
+    print("Q3a: ")
     print(X)
 
     #Q3b
     Y = []
 
-    for y in days:
-        Y.append(y)
+    for y in days: ## For each day
+        Y.append(y) ## Append the day to the list
 
-    Y = np.array(Y)
-    print("Q3b:")
+    Y = np.array(Y) ## Convert to numpy array
+
+    ## Print the result
+    print("Q3b: ")
     print(Y)
 
     #Q3c
-    X_t = np.transpose(X)
-    Z = np.dot(X_t,X)
-    print("Q3c:")
+    X_t = np.transpose(X) ## Transpose X
+    Z = np.dot(X_t, X) ## Multiply X transpose by X
+
+    ## Print the result
+    print("Q3c: ")
     print(Z)
 
     #Q3d
-    I = np.linalg.inv(Z)
-    print("Q3d:")
+    I = np.linalg.inv(Z) ## Invert Z
+
+    ## Print the result
+    print("Q3d: ")
     print(I)
 
     #Q3e 
-    PI = np.dot(I,X_t)
-    print("Q3e:")
+    PI = np.dot(I, X_t) ## Multiply I by X transpose
+
+    ## Print the result
+    print("Q3e: ")
     print(PI)
 
     #Q3f
-    hat_beta = np.dot(PI,Y)
-    print("Q3f:")
+    hat_beta = np.dot(PI, Y) ## Multiply PI by Y
+
+    ## Print the result
+    print("Q3f: ")
     print(hat_beta)
 
     #Q4
+    ## Initialize the list
     b_0 = hat_beta[0]
     b_1 = hat_beta[1]
     x_test = 2021
-    y_test = b_0 + np.dot(b_1,x_test)
-    print("Q4: " + str(y_test))
+    y_test = b_0 + np.dot(b_1, x_test)
+
+    print("Q4: " + str(y_test)) ## Print the result
 
     #Q5
-    if b_1 > 0:
+    if b_1 > 0: ## If b_1 is positive
         print("Q5a: >")
-        print("Q5b: The sign means that the number of winter days has an increasing trend along with years.")
-    elif b_1 < 0:
+        print("Q5b: The sign of greater than means that the number of winter days has an increasing (postive) trend along with years.")
+    elif b_1 < 0: ## If b_1 is negative
         print("Q5a: <")
-        print("Q5b: The sign means that the number of winter days has a decreasing trend along with years.")
-    else:
-        print("Q5a: =")
-        print("Q5b: The sign means that the number of winter days does not change along with years.")
+        print("Q5b: The sign of less than means that the number of winter days has a decreasing (negative) trend along with years.")
+    else: ## If b_1 is 0
+        print("Q5a: =") 
+        print("Q5b: The sign of equal to means that the number of winter days does not change along with years.")
 
     #Q6
-    x_star = -(b_0)/(b_1)
+    x_star = -(b_0) / (b_1) ## Calculate x_star
+
+    ## Print the result
     print("Q6a: "+ str(x_star))
     print("Q6b: We can see from the graph that the frozen days could be different between two consecutive years. That means it is possible for Mendota to freeze again after the prediction year. So I feel like the x* is not a perfect compelling prediction that states Mendota will no longer freeze." )
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1]) ## Run the main function
